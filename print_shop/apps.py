3f93d3c8ce661744353af3c14d2e411c63d8af93
Kwanusu <kwanusujoseph@gmail.com>
2026-03-08 17:47:16 +0300

    Initial commit

---
+++ print_shop/apps.py
@@ -0,0 +1,5 @@
+from django.apps import AppConfig
+
+
+class PrintShopConfig(AppConfig):
+    name = 'print_shop'
---
+++ print_shop/urls.py
@@ -0,0 +1,8 @@
+from django.conf.urls import path, include
+from rest_framework.routers import DefaultRouter
+
+router = DefaultRouter()
+
+urlpatterns = [
+    path('', include(router.urls)),
+]
